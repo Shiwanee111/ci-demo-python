4a733d927fe21745c4a46ded566e72b7dbd1663a
Shiwanee Pandey <shiwaneepandey111@gmail.com>
2026-01-20 00:56:26 -0800

    Trigger Jenkins build

--- app.py
+++ app.py
@@ -2,4 +2,11 @@ def add(a, b):
     return a + b
 
 if __name__ == "__main__":
+    print("CI build triggered via Jenkins")
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
     print("Sum:", add(2, 3))
+
+print("CI with Jenkins")
